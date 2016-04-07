1e1c9a444982540f72849452f038d445068fce6e
Antoine Dubé <antoine.dube@gmail.com>
2016-04-07 19:40:33 -0400

    Heroku deployment stuff.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ var morgan = require('morgan');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(morgan('combined'));
 
 app.use(express.static('templates'));
 app.use('/stylesheets', express.static('stylesheets'));
 
-app.listen(8000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port: ', app.get('port'));
+});
