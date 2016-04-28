4440cc1a39ae04b7dfd82617c1e24fcb7e94a267
Antoine Dubé <antoine.dube@gmail.com>
2016-04-28 15:34:37 -0400

    Added file-stream log.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var file_stream_rotator = require('file-stream-rotator');
 var morgan = require('morgan');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(morgan('combined'));
+var log_directory = __dirname + '/log'
+var access_log_stream = file_stream_rotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: log_directory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false
+});
+
+app.use(morgan('combined', { stream: access_log_stream }));
 
 app.use(express.static('templates'));
 app.use('/stylesheets', express.static('stylesheets'));
