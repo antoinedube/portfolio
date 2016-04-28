5324b79eea7be4c906e021afdd20cdaa1720d929
Antoine Dubé <antoine.dube@gmail.com>
2016-04-28 15:52:48 -0400

    Added eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+      "require": true
+    }
+}
