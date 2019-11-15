ee8577781c6142601c15124902fe4618cb4785d2
Antoine Dubé <antoine.dube@dti.ulaval.ca>
2019-11-15 16:33:41 -0500

    Quick update

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var express = require('express');
 var file_stream_rotator = require('file-stream-rotator');
 var morgan = require('morgan');
@@ -8,7 +6,7 @@ var app = express();
 
 app.set('port', 5000);
 
-var log_directory = '/log';
+var log_directory = 'log';
 var access_log_stream = file_stream_rotator.getStream({
     date_format: 'YYYYMMDD',
     filename: log_directory + '/access-%DATE%.log',
